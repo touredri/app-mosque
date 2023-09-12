d2662bc454c2c3cfef49493a7dcc40dc8a7a7c82
Isac <isacdiakite3@gmail.com>
2023-09-12 18:58:35 +0000

    firebase installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,29 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
 
+ 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule, 
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/service/radio.service.ts
+++ src/app/service/radio.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from "@angular/core";
-import { Radio } from "../model/radio.model";
+import { Radio } from "../models/radios.model";
+import { AngularFirestore } from "@angular/fire/compat/firestore";
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from "@angular/fire/compat/database";
 
 
 @Injectable({
     providedIn:'root'
 })
 export class RadioService{
+    listRadio!:AngularFireList<any>
+    radio!:AngularFireObject<any>
 
+    constructor(private firestore:AngularFireDatabase){}
     mesRadios:Radio[] = [
         {
             id:            1,
@@ -32,5 +37,41 @@ export class RadioService{
             id: this.mesRadios[this.mesRadios.length - 1].id + 1
         }
         this.mesRadios.push(radio)
+    }
+
+    onAddRadio(formValue: { nom: string,fluxAudio:string,frequence:number,logo:string,emplacement?:string ,langue?:string, description?: string }) {
+        return this.listRadio.push({
+            formValue,
+            id: this.mesRadios[this.mesRadios.length -1].id + 1
+        })
+    }
+
+    // getRadio(id:number){
+    //     this.radio= this.firestore.object('/radio/' + id)
+    //     return this.radio
+    // }
+
+    getAllRadios(){
+        this.listRadio = this.firestore.list('/radio')
+    }
+
+    editRadios(id:any ,radio:Radio){
+        return this.radio.update({
+            id: radio.id,
+            nom: radio.nom,
+            frequence: radio.frequence,
+            fluxAudio: radio.fluxAudio,
+            logo:radio.logo,
+            description : radio.description,
+            emplacement : radio.emplacement,
+            langue : radio.langue
+        
+        })
+    }
+
+
+    deleteRadio(id:number){
+        this.radio = this.firestore.object('/radio/'+ id)
+        this.radio.remove()
     }
 }
