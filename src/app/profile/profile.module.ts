309ea39b0be96ef7eaddcba7cdb657a564869b20
touredri <dcompte223@gmail.com>
2023-09-13 18:13:31 +0000

    profile update

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ProfilePageRoutingModule } from './profile-routing.module';
 
 import { ProfilePage } from './profile.page';
+import { Router } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ProfilePageRoutingModule
+    ProfilePageRoutingModule,
+    Router
   ],
   declarations: [ProfilePage]
 })
