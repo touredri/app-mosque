f62204d024a86467a1c50c0e33b49b6cb91c1f27
touredri <dcompte223@gmail.com>
2023-09-29 14:47:58 +0000

    dynamic time

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,7 +7,7 @@ import { FirestoreService } from '../service/firestore.service';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page  implements OnInit {
+export class Tab1Page implements OnInit {
   priereTime: any;
   time: any;
   currentTime: any;
@@ -18,24 +18,25 @@ export class Tab1Page  implements OnInit {
     '../../../assets/1.jpeg',
     '../../../assets/3.jpeg',
     '../../../assets/4.jpeg',
-    '../../../assets/mosque.jpg',
+    '../../../assets/Mosquee_mmm.jpg',
   ];
 
   constructor(
     private priereService: PriereService,
     private firebase: FirestoreService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     // this.firebase.init();
     this.priereService.checkNetworkConnection();
     this.firebase.getMosquees().subscribe((data: any) => {
-    this.data = data;
-    this.mosquee = [
+      this.data = data;
+      this.mosquee = [
         {
           id: this.data[1].info.id,
           src: this.data[1].image,
-          nom: this.data[1].info.nom  },
+          nom: this.data[1].info.nom
+        },
         {
           id: this.data[3].info.id,
           src: this.data[3].image,
@@ -44,8 +45,8 @@ export class Tab1Page  implements OnInit {
         {
           id: this.data[0].info.id,
           src: this.data[0].image,
-          nom: this.data[0].info.nom 
-        }      
+          nom: this.data[0].info.nom
+        }
       ]
     });
     // priere data part
@@ -56,18 +57,34 @@ export class Tab1Page  implements OnInit {
   }
 
   setCurrentTime(data: any) {
-    const time = new Date().getHours();
-    console.log(time);
-    if(0 < time && time < 6) {
-      this.priereTime = data.Fajr
-    } else if (6 < time && time <= 14) {
-      this.priereTime = data.Dhuhr
-    } else if (14 < time && time <= 16) {
-      this.priereTime = data.Asr
-    } else if (16 < time && time < 19) {
-      this.priereTime = data.Maghrib
-    } else {
-      this.priereTime = data.Isha
+    // Obtenez l'heure actuelle du dispositif (au format "HH:MM")
+    const heureActuelle = new Date().toLocaleTimeString('en-US', { hour12: false, hour: '2-digit', minute: '2-digit' });
+
+    // Convertissez l'heure actuelle en minutes
+    const [heuresActuelles, minutesActuelles] = heureActuelle.split(':');
+    const heureActuelleEnMinutes = parseInt(heuresActuelles, 10) * 60 + parseInt(minutesActuelles, 10);
+    // console.log(heuresActuelles + ' ' + heureActuelleEnMinutes);
+    // Initialisez des variables pour stocker l'heure la plus proche et la différence minimale
+    let heureLaPlusProche = '';
+    let differenceMinimale = Number.MAX_SAFE_INTEGER;
+
+    for (const timing in data) {
+      // console.log(data[timing].split(' ')[0]);
+      const heureTimings = data[timing].split(' ')[0]; // Extrait l'heure ("HH:MM") de la chaîne
+
+      const [heuresTimings, minutesTimings] = heureTimings.split(':');
+      const heureTimingsEnMinutes = parseInt(heuresTimings, 10) * 60 + parseInt(minutesTimings, 10);
+
+      const difference = Math.abs(heureActuelleEnMinutes - heureTimingsEnMinutes);
+
+      if (difference < differenceMinimale) {
+        differenceMinimale = difference;
+        heureLaPlusProche = data[timing].split(' ')[0];
+        this.priereTime = heureLaPlusProche;
+        this.time = timing;
+      }
     }
+    console.log("L'heure la plus proche est :", heureLaPlusProche);
+    
   }
 }
